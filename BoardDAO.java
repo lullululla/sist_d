2eff05cda555900842271cbb72fae22d52aa0bf3
lullulalla <jisoo950225@gmail.com>
2024-03-19 11:25:53 +0900

    BoardDAO 수정-insert,list [김지수]

--- BoardDAO.java
+++ BoardDAO.java
@@ -1,5 +1,13 @@
 class BoardDAO{
 	public void insert(){
+		System.out.println("BoardDAO");
 		System.out.println("게시물을 등록하였습니다.");
+	}
+	public List list(){
+		ArrayList list = new ArrayList();
+		list.add("1번 게시물");
+		list.add("2번 게시물");
+		list.add("3번 게시물");
+		return list;
 	}
 }
